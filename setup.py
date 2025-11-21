44bcd85691d4685f6c9ee4f166376fac3e9c4f34
Wan <103370604+wSanice@users.noreply.github.com>
2025-11-21 12:36:13 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='leblanc',
-    version='0.6.0',
+    version='0.6.1',
     packages=find_packages(),
     install_requires=[
         'pandas>=1.0.0',
